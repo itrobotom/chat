8d2851fb7932880225c88bca8899f8d6c8dfd515
itrobotom <121325608+itrobotom@users.noreply.github.com>
2023-05-10 07:31:23 +0700

    Chat ver.5
    Реализовал вывод времени при рендере истории сообщений. Обернул рендер сообщений в отдельную функцию, чтобы не работать с DOM через цикл в асинхронной функции.

--- js/message.js
+++ js/message.js
@@ -1,3 +1,4 @@
+import { format } from 'date-fns'
 
 import { TEMPLATE_MESSAGE, TEMPLATE_MESSAGE_USER, MESSAGE_CONTAINER, INPUT, URL } from './constants'
 import { myNameServer } from './nameUsers'
@@ -32,7 +33,7 @@ function sendMessage(e) {
 async function getHistoryMessage() {
     const token = getCodeCookie();
     try {
-        const response = await fetch(URL.HIST_MESSAGE ,{ 
+        const response = await fetch(URL.HIST_MESSAGE ,{ //response - это промис 
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8',
@@ -41,35 +42,39 @@ async function getHistoryMessage() {
         });
         if (response.ok) {
             let json = await response.json();
-            console.log(`Состояние: ${json.message}, Код ответа: ${json.cod}`);
             //console.log(`История 300 сообщений с сервера в json: ${JSON.stringify({json})}`);
             console.log(json.messages);
-            let allMessage = json.messages;
-            allMessage.forEach(element => {
-                //console.log(element);
-                console.log(element.user.name);
-                console.log(element.text);
-                //клонируем содержимое template
-                const cloneMessageTemplate = TEMPLATE_MESSAGE_USER.content.cloneNode(true);
-                // находим тег span нашего клона шаблона и помещаем туда текст сообщения из формы
-                cloneMessageTemplate.querySelector("#text-message").textContent = element.text;
-                //найти имя отправителя и установить имя отправителя
-                cloneMessageTemplate.querySelector("#my-name").textContent = `${element.user.name}: `;
-                // Добавить новое сообщение в элемент
-                MESSAGE_CONTAINER.append(cloneMessageTemplate);
-                MESSAGE_CONTAINER.scrollTop = MESSAGE_CONTAINER.scrollHeight;
-            });
+            renderHistoryMessage(json);       
         }
         else {
             console.log("Есть проблемы с получением истории сообщений!");
             //нужно открыть экран для авторизации, чтобы получить токен (раз его нет в куках)
-        }
-          
+        }  
     }
     //отловит ошибку, если, например, пропадет интернет
     catch(error) {
         alert(`Ошибка: ${error.name} - ${error.message}`);
     }
+}
+
+function renderHistoryMessage(jsonMessages) {
+    const allMessage = jsonMessages.messages;
+    allMessage.forEach(element => {
+        //console.log(element);
+        console.log(element.user.name);
+        console.log(element.text);
+        //клонируем содержимое template
+        const cloneMessageTemplate = TEMPLATE_MESSAGE_USER.content.cloneNode(true);
+        // находим тег span нашего клона шаблона и помещаем туда текст сообщения из формы
+        cloneMessageTemplate.querySelector("#text-message").textContent = element.text;
+        //найти имя отправителя и установить в шаблон
+        cloneMessageTemplate.querySelector("#my-name").textContent = `${element.user.name}: `;
+        //найти врем отправителя и установить в шаблон
+        cloneMessageTemplate.querySelector(".time-message").textContent = format(new Date(element.createdAt),"HH:mm");
+        // Добавить новое сообщение в элемент
+        MESSAGE_CONTAINER.append(cloneMessageTemplate);
+        MESSAGE_CONTAINER.scrollTop = MESSAGE_CONTAINER.scrollHeight;
+    });
 }
 
 function checkValidMessage() {
